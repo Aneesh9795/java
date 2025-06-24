77c2ae31c6dbef85992e745f34f5d492228e2152
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-06-24 16:40:03 +0530

    demo,student

---
+++ student.java
@@ -0,0 +1,18 @@
+public class student {
+
+public static void main(String[] args) {
+   int age = 20;
+        double height = 5.9;
+        char grade = 'A';
+        String name = "Amit";
+        boolean isPass = true;
+
+        System.out.println("Name " +name );
+        System.out.println("isPass" +isPass);
+        System.out.println("Grade" +grade);
+        System.out.println("Height" +height);
+        System.out.println("Age" +age);
+
+}    
+
+}
