b7d9368ae8f822a3956d825447d2a2be9200c818
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-06-30 18:21:34 +0530

    main

---
+++ Primitive.java
@@ -0,0 +1,21 @@
+public class Primitive {
+  public static void main(String[] args) {
+    int age = 25;
+    float height = 5.8f;
+    double salary = 99999.99;
+    char grade = 'A';
+    boolean isPass = true;
+    long population = 1380000000L;
+    short temp = 250;
+    byte level = 127;
+
+    System.out.println("Age: " + age);
+    System.out.println("Height: " + height);
+    System.out.println("Salary: " + salary);
+    System.out.println("Grade: " + grade);
+    System.out.println("Pass: " + isPass);
+    System.out.println("Population: " + population);
+    System.out.println("Temperature: " + temp);
+    System.out.println("Level: " + level);
+  }
+}
